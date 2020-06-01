0f78db45dba9727e6bda8e25710c323af8e5bff0
heuhiu <hieuvuong3562@gmail.com>
2020-06-01 14:11:23 +0700

    [hieuvm] Fixed store

--- src/index.js
+++ src/index.js
@@ -9,6 +9,7 @@ import appReducers from './reducers/index';
 import thunk from 'redux-thunk';
 
 const composeEnhancer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
 const store = createStore(
   appReducers,
   composeEnhancer(applyMiddleware(thunk)),
