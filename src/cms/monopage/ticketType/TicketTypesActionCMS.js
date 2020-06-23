dc13614dc650336c5db840296e18788ca6c187dc
heuhiu <hieuvuong3562@gmail.com>
2020-06-23 09:49:18 +0700

    [hieuvm_ticketTypes  ActionCMS_expand]

--- src/cms/monopage/ticketType/TicketTypesActionCMS.js
+++ src/cms/monopage/ticketType/TicketTypesActionCMS.js
@@ -3,7 +3,8 @@ import { Link } from 'react-router-dom';
 import { connect } from 'react-redux';
 import { actAddTicketTypeRequest, actUpdateTicketTypeRequest, actGetTicketTypeRequest } from '../../../actions/indexTicketTypes';
 import { actFetchPlacesRequest } from '../../../actions/indexPlaces';
-import { Form } from 'react-bootstrap'
+import { Form } from 'react-bootstrap';
+import Select from 'react-select'
 
 class TicketTypesActionCMS extends Component {
 
@@ -13,7 +14,11 @@ class TicketTypesActionCMS extends Component {
             id: '',
             txtName: '',
             txtShortDescription: '',
-            txtDetailDescription: ''
+            txtDetailDescription: '',
+            drbPlaceId: '',
+            loaded: false,
+            fetched: false,
+            customCheck: '',
         };
     }
 
@@ -23,6 +28,7 @@ class TicketTypesActionCMS extends Component {
             var id = match.params.id;
             this.props.onEditTicketType(id)
         } // else => add
+        this.props.fetchAllPlaces();
     }
 
     componentWillReceiveProps(nextProps) {
@@ -33,6 +39,10 @@ class TicketTypesActionCMS extends Component {
                 txtName: itemEditing.name,
                 txtShortDescription: itemEditing.shortDescription,
                 txtDetailDescription: itemEditing.detailDescription
+            })
+        } else {
+            this.setState({
+                fetched: true
             })
         }
     }
@@ -45,8 +55,23 @@ class TicketTypesActionCMS extends Component {
         this.setState({
             [name]: value
         });
+
+    }
+    onChangePlace = (e) => {
+        const { drbPlaceId } = this.state;
+        this.setState({
+            drbPlaceId: e.value
+        });
+        console.log(drbPlaceId);
     }
 
+    onChangePlace2 = (e) => {
+        const { customCheck } = this.state;
+        this.setState({
+            customCheck: e.value
+        });
+        console.log(customCheck);
+    }
     onSubmit = (e) => {
         e.preventDefault();
         var { id, txtName, txtShortDescription, txtDetailDescription } = this.state;
@@ -65,57 +90,111 @@ class TicketTypesActionCMS extends Component {
 
     render() {
         var { txtName, txtShortDescription, txtDetailDescription } = this.state;
-        return (
-            <div className="container">
-                <form onSubmit={this.onSubmit}>
-                    <legend>* Please enter full information</legend>
+        var { places } = this.props;
+        var { drbPlaceId, customCheck, loaded } = this.state;
+        var options = []
+        var renderOpt = drbPlaceId
+        if (places.length > 0 && this.state.fetched) {
+            for (let i = 0; i < places.length; i++) {
+                var option = { value: places[i].id, label: places[i].name }
+                options.push(option);
+                if (drbPlaceId === places[i].id) {
+                    renderOpt = i
+                }
+            }
+            loaded = true;
+        }
+        if (loaded) {
+            return (
+                <div className="container">
 
-                    {/* <Select
+                    <form onSubmit={this.onSubmit}>
+                        <legend>* Please enter full information</legend>
+
+                        {/* <Select
                         closeMenuOnSelect={false}
                         components={animatedComponents}
                         defaultValue={[colourOptions[4], colourOptions[5]]}
                         isMulti
                         options={colourOptions}
                     /> */}
-
-                    <div className="form-group">
-                        <label>Ticket Name </label>
-                        <input onChange={this.onChange} value={txtName} name="txtName" type="text" className="form-control" />
-                    </div>
-                    <div className="form-group">
-                        <label>Effective Time</label>
-                        <textarea onChange={this.onChange} value={txtShortDescription} name="txtShortDescription" className="form-control" rows="3">
-                        </textarea>
-                    </div>
-                    <div className="form-group">
-                        <label>Description</label>
-                        <textarea onChange={this.onChange} value={txtDetailDescription} name="txtDetailDescription" className="form-control" rows="3">
-                        </textarea>
-                    </div>
-                    <div className="form-group">
-                        <label>Cancel Policy</label>
-                        <textarea onChange={this.onChange} value={txtDetailDescription} name="txtDetailDescription" className="form-control" rows="3">
-                        </textarea>
-                    </div>
-                    <div className="form-group">
-                        <label>Reservation Infomation</label>
-                        <textarea onChange={this.onChange} value={txtDetailDescription} name="txtDetailDescription" className="form-control" rows="3">
-                        </textarea>
-                    </div>
-                    <div className="form-group">
-                        <label>Conversion Method</label>
-                        <textarea onChange={this.onChange} value={txtDetailDescription} name="txtDetailDescription" className="form-control" rows="3">
-                        </textarea>
-                    </div>
-                    <Link to="/ticketTypes" className="btn btn-danger mr-5">
-                        <i className="glyphicon glyphicon-arrow-left"></i> Back
-                    </Link>
-                    <button type="submit" className="btn btn-primary">
-                        <i className="glyphicon glyphicon-save"></i> Save Ticket Type
+                        <div className="myDiv">
+                            <label>Place Name </label>
+                            <div >
+                                <Select
+                                    options={options}
+                                    defaultValue={options[renderOpt]}
+                                    onChange={this.onChangePlace}
+                                />
+                            </div>
+                        </div>
+                        <div style={{ display: drbPlaceId ? "" : "none" }} className="myDiv">
+                            <label>id u pick: {drbPlaceId} </label>
+                            <div  >
+                                {/* thứ mà hiện ra sau khi chọn dropdown */}
+                                <div className="myDiv">
+                                    <label>Place Name </label>
+                                    <div >
+                                        <Select
+                                            options={options}
+                                            defaultValue={options[renderOpt]}
+                                            onChange={this.onChangePlace2}
+                                        />
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div style={{ display: customCheck ? "" : "none" }}>
+                            <label>id u pick: {customCheck} </label>
+                            <div className="form-group">
+                                <label>Ticket Name </label>
+                                <input onChange={this.onChange} value={txtName} name="txtName" type="text" className="form-control" />
+                            </div>
+                            <div className="form-group">
+                                <label>Effective Time</label>
+                                <textarea onChange={this.onChange} value={txtShortDescription} name="txtShortDescription" className="form-control" rows="3">
+                                </textarea>
+                            </div>
+                            <div className="form-group">
+                                <label>Description</label>
+                                <textarea onChange={this.onChange} value={txtDetailDescription} name="txtDetailDescription" className="form-control" rows="3">
+                                </textarea>
+                            </div>
+                            <div className="form-group">
+                                <label>Cancel Policy</label>
+                                <textarea onChange={this.onChange} value={txtDetailDescription} name="txtDetailDescription" className="form-control" rows="3">
+                                </textarea>
+                            </div>
+                            <div className="form-group">
+                                <label>Reservation Infomation</label>
+                                <textarea onChange={this.onChange} value={txtDetailDescription} name="txtDetailDescription" className="form-control" rows="3">
+                                </textarea>
+                            </div>
+                            <div className="form-group">
+                                <label>Conversion Method</label>
+                                <textarea
+                                    value={txtDetailDescription}
+                                    name="txtDetailDescription"
+                                    className="form-control"
+                                    rows="3"
+                                    onBlur={this.handleBlur}
+                                >
+                                </textarea>
+                            </div>
+                        
+                            <Link to="/ticketTypes" className="btn btn-danger mr-5">
+                                <i className="glyphicon glyphicon-arrow-left"></i> Back
+                            </Link>
+                            <button type="submit" className="btn btn-primary">
+                                <i className="glyphicon glyphicon-save"></i> Save Ticket Type
                             </button>
-                </form>
-            </div>
-        );
+                            </div>
+                    </form>
+                </div>
+            );
+        } else {
+            return ""
+        }
     }
 
 }
@@ -135,7 +214,7 @@ const mapDispatchToProps = (dispatch, props) => {
         onUpdateTicketType: (city) => {
             dispatch(actUpdateTicketTypeRequest(city, props.history));
         },
-        onGetTicketType: (id) => {
+        onEditTicketType: (id) => {
             dispatch(actGetTicketTypeRequest(id));
         },
         fetchAllPlaces: () => {
